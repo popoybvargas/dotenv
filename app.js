844d4775a5272f67e668dd04211774918f15a865
Popoy Vargas <popoyvargas@popoyMBA.local>
2020-06-17 19:05:45 +0800

    initial

---
+++ app.js
@@ -0,0 +1,32 @@
+const fs = require( 'fs' );
+
+module.exports = ( customPath = null ) =>
+{
+	let path = customPath;
+
+	if ( ! path )
+	{
+		if ( ! fs.existsSync( `${__dirname}/.env` ) )
+		{
+			fs.writeFileSync( `${__dirname}/.env`, '', err => console.log( 'File created.' ) );
+		}
+		path = `${__dirname}/.env`;
+	}
+	const configurations = fs.readFileSync( path, 'utf-8' );
+	let configurationsArray = [];
+	let configurationsObject = {};
+
+	if ( configurations )
+	{
+		configurationsArray = configurations.split( '\n' );
+		configurationsArray.forEach( config =>
+		{
+			const key = config.substr( 0, config.indexOf( '=' ) );
+			const value = config.substr( config.indexOf( '=' ) + 1 );
+
+			if ( key ) { configurationsObject[ key ] = value; }
+		});
+	}
+
+	return configurationsObject;
+}

b800c00a566ec769638c885f9ea8954bccab2396
Popoy Vargas <popoyvargas@popoyMBA.local>
2020-06-18 09:43:55 +0800

    Rename package and update README accordingly.

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# zv-getconfig
+# load.env
 Get configuration file and load values.
 
 #### Basic Usage
 ```
-require( 'zv-getconfig' )();
+require( 'load.env' )();
 ```
 The above code loads configurations specified in the ***.env*** file in the project's root directory.
 > Use the above code as soon as possible (topmost part) in the application's *main* script.
 
 To load configurations from a custom file, specify its path as an argument, e.g.
 ```
-require( 'zv-getconfig' )( PATH_TO_CUSTOM_CONFIG_FILE );
+require( 'load.env' )( PATH_TO_CUSTOM_CONFIG_FILE );
 ```
 
 Configurations will now be loaded as part of the appliation's environment variables ***process.env***. To access configurations, e.g. 
--- app.js
+++ app.js
@@ -8,14 +8,14 @@ module.exports = ( customPath = null ) =>
 	{
 		if ( ! fs.existsSync( '.env' ) )
 		{
-			fs.writeFileSync( '.env', '', err => console.log( 'File created.' ) );
-			return console.log( 'Configuratons not available! Please provide them in the .env file.' );
+			fs.writeFileSync( '.env', '', err => console.log( '.env file created.' ) );
+
+			return console.log( 'Configuratons not available! Please specify them in the .env file.' );
 		}
 		path = '.env';
 	}
 	const configurations = fs.readFileSync( path, 'utf-8' );
 	let configurationsArray = [];
-	// let configurationsObject = {};
 
 	if ( configurations )
 	{
@@ -25,14 +25,11 @@ module.exports = ( customPath = null ) =>
 			const key = config.substr( 0, config.indexOf( '=' ) );
 			const value = config.substr( config.indexOf( '=' ) + 1 );
 
-			// if ( key ) { configurationsObject[ key ] = value; }
 			if ( key ) { process.env[ key ] = value; }
 		});
 	}
 	else
 	{
-		console.log( 'No available configuraton! Please provide them in the .env file.' );
+		console.log( 'No available configuraton! Please specify them in the .env file.' );
 	}
-
-	// return configurationsObject;
 }

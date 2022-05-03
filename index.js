fdaa4a21b98bb19ec050e611675c20c383461985
Popoy Vargas <jaime.vargas@accenture.com>
2022-05-03 17:38:01 +0800

    [FIX] Bug with parsing configurations.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = customEnvFilePath =>
 	if (customEnvFilePath && fs.existsSync(customEnvFilePath)) envFilePath = customEnvFilePath;
 	else if (!fs.existsSync(envFilePath))
 	{
-		console.warn('Configuratons not available! Please specify them in the .env file.');
+		console.warn('Configurations not available! Please specify them in the .env file.');
 
 		return fs.writeFileSync(envFilePath, '', () => console.log('.env file created.'))
 	}
@@ -23,16 +23,16 @@ module.exports = customEnvFilePath =>
 	{
 		configurationsArray = configurations.split('\n');
 
-		configurationsArray.forEach(config =>
+		for (const config of configurationsArray)
 		{
 			const indexOfEqual = config.indexOf('=');
 			
 			if (indexOfEqual >= 0)
 			{
-				const key = config.substring(0, indexOfEqual);
+				let key = config.substring(0, indexOfEqual);
 				process.env[key] = config.substring(indexOfEqual + 1);
 			}
-		});
+		}
 	}
 	else console.error(`No available configuraton in ${envFilePath}!`);
 };
